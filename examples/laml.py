70a224b26a729a3e856bb29d3714ad2e56d5218c
Luca Pradovera <luca.pradovera@gmail.com>
2018-10-02 19:25:24 +0200

    Fix setup.py

---
+++ examples/laml.py
@@ -0,0 +1,6 @@
+from signalwire.voice_response import VoiceResponse
+
+r = VoiceResponse()
+r.say("Welcome to sw!")
+print(str(r))
+
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='signalwire',
-      version='1.0.0-rc1',
+      version='1.0.0-rc2',
       description='Provides SignalWire LAML and REST functionality',
       url='https://github.com/signalwire/signalwire-python',
       author='SignalWire Team',
       author_email='open.source@signalwire.com',
       license='MIT',
-      packages=['signalwire'],
+      packages=find_packages(exclude=['tests', 'tests.*']),
       install_requires=[
           'twilio',
       ],
