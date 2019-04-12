60582c0ba47d1e762272f5c4d25648f737b071af
Luca Pradovera <luca.pradovera@gmail.com>
2019-04-12 11:06:39 +0200

    Allow setting Space URL via SIGNALWIRE_SPACE_URL (#13)

--- signalwire/tests/test_sdk.py
+++ signalwire/tests/test_sdk.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+import os
 
 class TestConfigurable(TestCase):
   def test_api_url(self):
@@ -10,5 +11,12 @@ class TestConfigurable(TestCase):
   def test_api_url_scheme(self):
     from signalwire.rest import Client as signalwire_client
     client = signalwire_client('account', 'token', signalwire_space_url = 'https://myname.signalwire.com')
+
+    self.assertEqual(client.api.base_url, 'https://myname.signalwire.com')
+  
+  def test_api_url_environnment(self):
+    from signalwire.rest import Client as signalwire_client
+    os.environ['SIGNALWIRE_SPACE_URL'] = 'myname.signalwire.com'
+    client = signalwire_client('account', 'token')
 
     self.assertEqual(client.api.base_url, 'https://myname.signalwire.com')
