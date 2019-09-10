540c21dce7080c0f5529288ef8083a5a4f5ca477
Edoardo Gallo <edo91.gallo@gmail.com>
2019-09-10 15:25:16 +0200

    fix indentation

--- signalwire/blade/messages/connect.py
+++ signalwire/blade/messages/connect.py
@@ -2,21 +2,21 @@ from signalwire.blade.messages.message import Message
 
 class Connect(Message):
 
-    MAJOR = 2
-    MINOR = 1
-    REVISION = 0
+  MAJOR = 2
+  MINOR = 1
+  REVISION = 0
 
-    def __init__(self, project, token):
-        self.method = 'blade.connect'
-        params = {
-            'version': {
-                'major': self.MAJOR,
-                'minor': self.MINOR,
-                'revision': self.REVISION
-            },
-            'authentication': {
-                'project': project,
-                'token': token
-            }
-        }
-        super().__init__(params=params)
+  def __init__(self, project, token):
+    self.method = 'blade.connect'
+    params = {
+      'version': {
+        'major': self.MAJOR,
+        'minor': self.MINOR,
+        'revision': self.REVISION
+      },
+      'authentication': {
+        'project': project,
+        'token': token
+      }
+    }
+    super().__init__(params=params)
