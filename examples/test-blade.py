321bbb5b08b8da835749c940401c27e361076bb1
Edoardo Gallo <edo91.gallo@gmail.com>
2019-09-27 00:17:02 +0200

    relay test scrip

--- examples/test-blade.py
+++ examples/test-blade.py
@@ -1,10 +1,29 @@
+import asyncio
 import os
 from signalwire.relay.client import Client
 
-def ready(client):
+async def inbound_call(call):
+  result = await call.answer()
+  if result.successful:
+    print('Inbound call answered successful')
+    await asyncio.sleep(5)
+    hangup = await call.hangup()
+    if hangup.successful:
+      print('Call hangup')
+    else:
+      print('Call hangup failed')
+  else:
+    print('Inbound call failed or not answered')
+
+async def ready(client):
   print('Client ready!')
-  print(client.calling)
-  pass
+  await client.calling.receive(['home', 'office'], inbound_call)
+  # call = client.calling.new_call(from_number='+1xxx', to_number='+1yyy')
+  # result = await call.dial()
+  # if result.successful:
+  #   print('Outbound call answered successful')
+  # else:
+  #   print('Outbound call failed or not answered')
 
 def main():
   project = os.getenv('PROJECT', '')
