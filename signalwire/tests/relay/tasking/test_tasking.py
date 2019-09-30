46256747a8420d1968647065e23b6afbd13e462f
Edoardo Gallo <edo91.gallo@gmail.com>
2019-09-30 18:45:02 +0200

    add assertions

--- signalwire/tests/relay/tasking/test_tasking.py
+++ signalwire/tests/relay/tasking/test_tasking.py
@@ -10,8 +10,8 @@ async def test_on_receive(relay_tasking):
   message = Message.from_json('{"jsonrpc":"2.0","id":"uuid","method":"blade.broadcast","params":{"broadcaster_nodeid":"uuid","protocol":"signalwire-proto-test","channel":"notifications","event":"queuing.relay.tasks","params":{"project_id":"project-uuid","space_id":"space-uuid","context":"office","message":{"key":"value","data":"random stuff"},"timestamp":1569859833,"event_channel":"signalwire-proto-test"}}}')
   relay_tasking.client.message_handler(message)
 
-  # call = handler.call_args[0][0]
-  # assert isinstance(call, Call)
-  # assert call.from_number == '+12029999999'
-  # assert call.to_number == '+12028888888'
+  message = handler.call_args[0][0]
+  assert isinstance(message, dict)
+  assert message['key'] == 'value'
+  assert message['data'] == 'random stuff'
   handler.assert_called_once()
