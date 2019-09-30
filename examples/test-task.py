eae38f2e993946c1d5aa5e9b36bc5fc756bd4aeb
Edoardo Gallo <edo91.gallo@gmail.com>
2019-09-30 17:59:52 +0200

    Task class to deliver jobs in relay

---
+++ examples/test-task.py
@@ -0,0 +1,15 @@
+import asyncio
+import os
+from signalwire.relay.task import Task
+
+def main():
+  project = os.getenv('PROJECT', '')
+  token = os.getenv('TOKEN', '')
+  task = Task(project=project, token=token)
+  success = task.deliver('office', { 'key': 'value', 'data': 'random stuff' })
+  if success:
+    print('Task delivered')
+  else:
+    print('Task NOT delivered')
+
+main()
