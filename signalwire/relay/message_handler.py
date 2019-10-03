d9e71e1222bf28b5ead5abe1aa8dd81b70195791
Edoardo Gallo <edo91.gallo@gmail.com>
2019-10-03 16:49:11 +0200

    use constants for the blade.methods

--- signalwire/relay/message_handler.py
+++ signalwire/relay/message_handler.py
@@ -1,10 +1,13 @@
 import logging
+from .constants import BladeMethod
 
 def handle_inbound_message(client, message):
-  if message.method == 'blade.netcast':
+  if message.method == BladeMethod.NETCAST:
     pass
-  elif message.method == 'blade.broadcast':
+  elif message.method == BladeMethod.BROADCAST:
     _blade_broadcast(client, message.params)
+  elif message.method == BladeMethod.DISCONNECT:
+    client._idle = True
 
 def _blade_broadcast(client, params):
   if client.protocol != params['protocol']:
