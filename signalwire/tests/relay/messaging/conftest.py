17dde99a943fac8fd1fee3bd9fee1e5d82465d9d
Edoardo Gallo <edo91.gallo@gmail.com>
2019-10-04 14:45:57 +0200

    Messaging unit tests

---
+++ signalwire/tests/relay/messaging/conftest.py
@@ -0,0 +1,10 @@
+import json
+import pytest
+from signalwire.tests import MockedConnection, AsyncMock
+
+@pytest.fixture(scope='function')
+def relay_messaging(relay_client):
+  relay_client.protocol = 'signalwire-proto-test'
+  response = json.loads('{"requester_nodeid":"uuid","responder_nodeid":"uuid","result":{"code":"200","message":"Receiving all inbound related to the requested relay contexts and available scopes"}}')
+  relay_client.execute = AsyncMock(return_value=response)
+  return relay_client.messaging
