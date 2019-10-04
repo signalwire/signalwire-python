8a5d929f62954c17569055a7bc471c1ab7cb3a5e
Edoardo Gallo <edo91.gallo@gmail.com>
2019-10-04 19:25:10 +0200

    setup unit tests for call with missing hangup/dial/answer

--- signalwire/tests/relay/calling/test_call.py
+++ signalwire/tests/relay/calling/test_call.py
@@ -1,10 +1,11 @@
+import asyncio
 import json
 import pytest
 from signalwire.relay.calling import Call
 
 @pytest.fixture()
 def relay_call(relay_calling):
-  params = json.loads('{"call_state":"created","context":"office","device":{"type":"phone","params":{"from_number":"+12029999999","to_number":"+12028888888"}},"direction":"inbound","call_id":"call-id","node_id":"node-id"}')
+  params = json.loads('{"call_state":"created","context":"office","device":{"type":"phone","params":{"from_number":"+12029999999","to_number":"+12028888888"}},"direction":"inbound","call_id":"call-id","node_id":"node-id","tag":"call-tag"}')
   return Call(calling=relay_calling, **params)
 
 def test_init_options(relay_call):
@@ -19,3 +20,63 @@ def test_init_options(relay_call):
 
 def test_device(relay_call):
   assert relay_call.device == {'type':'phone','params':{'from_number':'+12029999999','to_number':'+12028888888'}}
+
+async def _fire(calling, notification):
+  calling.notification_handler(notification)
+
+@pytest.mark.asyncio
+async def test_answer_with_success(success_response, relay_call):
+  relay_call.calling.client.execute = success_response
+  payload = json.loads('{"event_type":"calling.call.state","event_channel":"signalwire-proto-test","timestamp":1570204684.1133151,"project_id":"project-uuid","space_id":"space-uuid","params":{"call_state":"answered","direction":"outbound","device":{"type":"phone","params":{"from_number":"+12029999999","to_number":"+12028888888"}},"call_id":"call-id","node_id":"node-id","tag":"call-tag"}}')
+  asyncio.create_task(_fire(relay_call.calling, payload))
+  result = await relay_call.answer()
+
+  assert result.successful
+  msg = relay_call.calling.client.execute.mock.call_args[0][0]
+  assert msg.params == json.loads('{"protocol":"signalwire-proto-test","method":"calling.answer","params":{"call_id":"call-id","node_id":"node-id"}}')
+  relay_call.calling.client.execute.mock.assert_called_once()
+
+@pytest.mark.asyncio
+async def test_answer_with_failure(fail_response, relay_call):
+  relay_call.calling.client.execute = fail_response
+  result = await relay_call.answer()
+  assert not result.successful
+  relay_call.calling.client.execute.mock.assert_called_once()
+
+@pytest.mark.asyncio
+async def test_hangup(success_response, relay_call):
+  relay_call.calling.client.execute = success_response
+  payload = json.loads('{"event_type":"calling.call.state","event_channel":"signalwire-proto-test","timestamp":1570204684.1133151,"project_id":"project-uuid","space_id":"space-uuid","params":{"call_state":"ended","direction":"outbound","device":{"type":"phone","params":{"from_number":"+12029999999","to_number":"+12028888888"}},"end_reason":"noAnswer","call_id":"call-id","node_id":"node-id","tag":"call-tag"}}')
+  asyncio.create_task(_fire(relay_call.calling, payload))
+  result = await relay_call.hangup()
+
+  assert result.successful
+  msg = relay_call.calling.client.execute.mock.call_args[0][0]
+  assert msg.params == json.loads('{"protocol":"signalwire-proto-test","method":"calling.end","params":{"call_id":"call-id","node_id":"node-id","reason":"hangup"}}')
+  relay_call.calling.client.execute.mock.assert_called_once()
+
+@pytest.mark.asyncio
+async def test_hangup_with_failure(fail_response, relay_call):
+  relay_call.calling.client.execute = fail_response
+  result = await relay_call.hangup()
+  assert not result.successful
+  relay_call.calling.client.execute.mock.assert_called_once()
+
+@pytest.mark.asyncio
+async def test_dial(success_response, relay_call):
+  relay_call.calling.client.execute = success_response
+  payload = json.loads('{"event_type":"calling.call.state","event_channel":"signalwire-proto-test","timestamp":1570204684.1133151,"project_id":"project-uuid","space_id":"space-uuid","params":{"call_state":"answered","direction":"outbound","device":{"type":"phone","params":{"from_number":"+12029999999","to_number":"+12028888888"}},"end_reason":"noAnswer","call_id":"call-id","node_id":"node-id","tag":"call-tag"}}')
+  asyncio.create_task(_fire(relay_call.calling, payload))
+  result = await relay_call.dial()
+
+  assert result.successful
+  msg = relay_call.calling.client.execute.mock.call_args[0][0]
+  assert msg.params == json.loads('{"protocol":"signalwire-proto-test","method":"calling.begin","params":{"tag":"'+relay_call.tag+'","device":{"type":"phone","params":{"from_number":"+12029999999","to_number":"+12028888888"}}}}')
+  relay_call.calling.client.execute.mock.assert_called_once()
+
+@pytest.mark.asyncio
+async def test_dial_with_failure(fail_response, relay_call):
+  relay_call.calling.client.execute = fail_response
+  result = await relay_call.dial()
+  assert not result.successful
+  relay_call.calling.client.execute.mock.assert_called_once()
