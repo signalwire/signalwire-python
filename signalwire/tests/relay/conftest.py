8de16c3a4a7971b3ca3a6932e931993c4ea9d270
Edoardo Gallo <edo91.gallo@gmail.com>
2019-10-04 09:45:59 +0200

    complete tests for client blade.disconnect

--- signalwire/tests/relay/conftest.py
+++ signalwire/tests/relay/conftest.py
@@ -1,7 +1,20 @@
+import asyncio
 import pytest
-from signalwire.tests import MockedConnection
+from signalwire.tests import MockedConnection, AsyncMock
 
 @pytest.fixture(scope='function')
-def relay_client():
+def relay_client(event_loop):
   from signalwire.relay.client import Client
-  return Client(project='project', token='token', connection=MockedConnection)
+  client = Client(project='project', token='token', connection=MockedConnection)
+  client.loop = event_loop
+  return client
+
+@pytest.fixture(scope='function')
+def relay_client_to_connect(relay_client):
+  asyncio.sleep = AsyncMock() # Mock sleep
+  relay_client.connection.responses = [
+    '{"jsonrpc":"2.0","id":"uuid","result":{"session_restored":false,"sessionid":"87cf6699-7a89-4491-b732-b51144155d46","nodeid":"uuid_node","master_nodeid":"00000000-0000-0000-0000-000000000000","authorization":{"project":"project","expires_at":null,"scopes":["calling"],"signature":"random_signature"},"routes":[],"protocols":[],"subscriptions":[],"authorities":[],"authorizations":[],"accesses":[],"protocols_uncertified":["signalwire"]}}',
+    '{"jsonrpc":"2.0","id":"uuid","result":{"requester_nodeid":"uuid","responder_nodeid":"uuid","result":{"protocol":"signalwire_random_proto"}}}',
+    '{"jsonrpc":"2.0","id":"uuid","result":{"protocol":"signalwire_random_proto","command":"add","subscribe_channels":["notifications"]}}'
+  ]
+  return relay_client
--- signalwire/tests/relay/test_client.py
+++ signalwire/tests/relay/test_client.py
@@ -1,26 +1,40 @@
 import asyncio
 import pytest
+from signalwire.blade.messages.message import Message
+from signalwire.blade.messages.execute import Execute
 from signalwire.relay.client import Client
 from unittest.mock import Mock
 from signalwire.tests import AsyncMock, MockedConnection
 
 @pytest.mark.asyncio
-async def test_example():
-  asyncio.sleep = AsyncMock() # Mock sleep
-  responses = [
-    '{"jsonrpc":"2.0","id":"uuid","result":{"session_restored":false,"sessionid":"87cf6699-7a89-4491-b732-b51144155d46","nodeid":"uuid_node","master_nodeid":"00000000-0000-0000-0000-000000000000","authorization":{"project":"project","expires_at":null,"scopes":["calling"],"signature":"random_signature"},"routes":[],"protocols":[],"subscriptions":[],"authorities":[],"authorizations":[],"accesses":[],"protocols_uncertified":["signalwire"]}}',
-    '{"jsonrpc":"2.0","id":"uuid","result":{"requester_nodeid":"uuid","responder_nodeid":"uuid","result":{"protocol":"signalwire_random_proto"}}}',
-    '{"jsonrpc":"2.0","id":"uuid","result":{"protocol":"signalwire_random_proto","command":"add","subscribe_channels":["notifications"]}}'
-  ]
-  client = Client(project='project', token='token', connection=MockedConnection)
-  client.connection.responses = responses
+async def test_connect(relay_client_to_connect):
   ready_callback = Mock()
-  client.on('ready', ready_callback)
-  await client._connect()
+  relay_client_to_connect.on('ready', ready_callback)
+  await relay_client_to_connect._connect()
 
   pending_tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
   await asyncio.gather(*pending_tasks)
 
-  assert client.session_id == '87cf6699-7a89-4491-b732-b51144155d46'
-  assert client.protocol == 'signalwire_random_proto'
+  assert relay_client_to_connect.session_id == '87cf6699-7a89-4491-b732-b51144155d46'
+  assert relay_client_to_connect.protocol == 'signalwire_random_proto'
   ready_callback.assert_called_once()
+
+@pytest.mark.asyncio
+async def test_blade_disconnect(relay_client_to_connect):
+  assert relay_client_to_connect._idle == False
+  blade_disconnect = Message.from_json('{"id":"378d7dea-e581-4305-a7e7-d29173797f32","jsonrpc":"2.0","method":"blade.disconnect","params":{}}')
+  relay_client_to_connect.message_handler(blade_disconnect)
+  assert relay_client_to_connect._idle == True
+
+  async def _reconnect():
+    assert relay_client_to_connect._executeQueue.qsize() == 1
+    relay_client_to_connect.connection.responses.append('{"jsonrpc":"2.0","id":"uuid","result":{"test":"done"}}')
+    await relay_client_to_connect._connect()
+
+  asyncio.create_task(_reconnect())
+  message = Execute({ 'protocol': 'fake', 'method': 'testing', 'params': {} })
+  result = await relay_client_to_connect.execute(message)
+
+  assert relay_client_to_connect._idle == False
+  assert relay_client_to_connect._executeQueue.qsize() == 0
+  assert result['test'] == 'done'
