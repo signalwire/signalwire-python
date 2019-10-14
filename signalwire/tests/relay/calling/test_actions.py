d8e0be77559c385f539a4566cfd412011c682bea
Edoardo Gallo <edo91.gallo@gmail.com>
2019-10-14 17:44:10 +0200

    write tests for RecordAction

--- signalwire/tests/relay/calling/test_actions.py
+++ signalwire/tests/relay/calling/test_actions.py
@@ -4,17 +4,26 @@ import pytest
 from unittest.mock import Mock, patch
 from signalwire.relay.calling.components.play import Play
 from signalwire.relay.calling.actions.play_action import PlayAction
+from signalwire.relay.calling.components.record import Record
+from signalwire.relay.calling.actions.record_action import RecordAction
 
 PLAY_STOP_PAYLOAD = json.loads('{"protocol":"signalwire-proto-test","method":"calling.play.stop","params":{"call_id":"call-id","node_id":"node-id","control_id":"control-id"}}')
 PLAY_PAUSE_PAYLOAD = json.loads('{"protocol":"signalwire-proto-test","method":"calling.play.pause","params":{"call_id":"call-id","node_id":"node-id","control_id":"control-id"}}')
 PLAY_RESUME_PAYLOAD = json.loads('{"protocol":"signalwire-proto-test","method":"calling.play.resume","params":{"call_id":"call-id","node_id":"node-id","control_id":"control-id"}}')
 PLAY_VOLUME_PAYLOAD = json.loads('{"protocol":"signalwire-proto-test","method":"calling.play.volume","params":{"call_id":"call-id","node_id":"node-id","control_id":"control-id","volume":4.1}}')
+RECORD_STOP_PAYLOAD = json.loads('{"protocol":"signalwire-proto-test","method":"calling.record.stop","params":{"call_id":"call-id","node_id":"node-id","control_id":"control-id"}}')
 
 @pytest.fixture()
 def play_action(relay_call):
   component = Play(relay_call, [{'type': 'audio', 'url': 'audio.mp3'}])
   component.control_id = 'control-id' # force-mock control_id
   return PlayAction(component)
+
+@pytest.fixture()
+def record_action(relay_call):
+  component = Record(relay_call, beep=True, terminators='#')
+  component.control_id = 'control-id' # force-mock control_id
+  return RecordAction(component)
 
 @pytest.mark.asyncio
 async def test_play_action_stop_with_success(success_response, relay_call, play_action):
@@ -86,4 +95,22 @@ async def test_play_action_volume_with_failure(fail_response, relay_call, play_a
   assert not result.successful
   msg = relay_call.calling.client.execute.mock.call_args[0][0]
   assert msg.params == PLAY_VOLUME_PAYLOAD
+  relay_call.calling.client.execute.mock.assert_called_once()
+
+@pytest.mark.asyncio
+async def test_record_action_stop_with_success(success_response, relay_call, record_action):
+  relay_call.calling.client.execute = success_response
+  result = await record_action.stop()
+  assert result.successful
+  msg = relay_call.calling.client.execute.mock.call_args[0][0]
+  assert msg.params == RECORD_STOP_PAYLOAD
+  relay_call.calling.client.execute.mock.assert_called_once()
+
+@pytest.mark.asyncio
+async def test_record_action_stop_with_failure(fail_response, relay_call, record_action):
+  relay_call.calling.client.execute = fail_response
+  result = await record_action.stop()
+  assert not result.successful
+  msg = relay_call.calling.client.execute.mock.call_args[0][0]
+  assert msg.params == RECORD_STOP_PAYLOAD
   relay_call.calling.client.execute.mock.assert_called_once()
