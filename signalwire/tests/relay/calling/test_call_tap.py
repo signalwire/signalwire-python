4958af9a66ecc92a2ce14436b6c8ddf2277b5012
Edoardo Gallo <edo91.gallo@gmail.com>
2019-10-16 17:30:20 +0200

    fix tests with int values

--- signalwire/tests/relay/calling/test_call_tap.py
+++ signalwire/tests/relay/calling/test_call_tap.py
@@ -19,7 +19,7 @@ def test_tap_events(relay_call):
   mock = Mock()
   relay_call.on('tap.stateChange', mock)
   relay_call.on('tap.finished', mock)
-  payload = json.loads('{"event_type":"calling.call.tap","params":{"control_id":"control-id","call_id":"call-id","node_id":"node-id","state":"finished","tap":{"type":"audio","params":{"direction":"listen"}},"device":{"type":"rtp","params":{"addr":"127.0.0.1","port":"1234","codec":"PCMU","ptime":"20"}}}}')
+  payload = json.loads('{"event_type":"calling.call.tap","params":{"control_id":"control-id","call_id":"call-id","node_id":"node-id","state":"finished","tap":{"type":"audio","params":{"direction":"listen"}},"device":{"type":"rtp","params":{"addr":"127.0.0.1","port":1234,"codec":"PCMU","ptime":20}}}}')
   relay_call.calling.notification_handler(payload)
   assert mock.call_count == 2
 
@@ -27,15 +27,15 @@ def test_tap_events(relay_call):
 async def test_tap_with_success(tap_success_response, relay_call):
   with patch('signalwire.relay.calling.components.uuid4', mock_uuid):
     relay_call.calling.client.execute = tap_success_response
-    payload = json.loads('{"event_type":"calling.call.tap","params":{"control_id":"control-id","call_id":"call-id","node_id":"node-id","state":"finished","tap":{"type":"audio","params":{"direction":"listen"}},"device":{"type":"rtp","params":{"addr":"127.0.0.1","port":"1234","codec":"PCMU","ptime":"20"}}}}')
+    payload = json.loads('{"event_type":"calling.call.tap","params":{"control_id":"control-id","call_id":"call-id","node_id":"node-id","state":"finished","tap":{"type":"audio","params":{"direction":"listen"}},"device":{"type":"rtp","params":{"addr":"127.0.0.1","port":1234,"codec":"PCMU","ptime":20}}}}')
     asyncio.create_task(_fire(relay_call.calling, payload))
     result = await relay_call.tap(audio_direction='listen', target_type='rtp', target_addr='127.0.0.1', target_port='1234')
     assert result.successful
     assert result.source_device == json.loads('{"type":"rtp","params":{"addr":"10.10.10.10","port":30030,"codec":"PCMU","ptime":20,"rate":8000}}')
-    assert result.destination_device == json.loads('{"type":"rtp","params":{"addr":"127.0.0.1","port":"1234","codec":"PCMU","ptime":"20"}}')
+    assert result.destination_device == json.loads('{"type":"rtp","params":{"addr":"127.0.0.1","port":1234,"codec":"PCMU","ptime":20}}')
     assert result.event.payload['state'] == 'finished'
     msg = relay_call.calling.client.execute.mock.call_args[0][0]
-    assert msg.params == json.loads('{"protocol":"signalwire-proto-test","method":"calling.tap","params":{"call_id":"call-id","node_id":"node-id","control_id":"control-id","tap":{"type":"audio","params":{"direction":"listen"}},"device":{"type":"rtp","params":{"addr":"127.0.0.1","port":"1234"}}}}')
+    assert msg.params == json.loads('{"protocol":"signalwire-proto-test","method":"calling.tap","params":{"call_id":"call-id","node_id":"node-id","control_id":"control-id","tap":{"type":"audio","params":{"direction":"listen"}},"device":{"type":"rtp","params":{"addr":"127.0.0.1","port":1234}}}}')
     relay_call.calling.client.execute.mock.assert_called_once()
 
 @pytest.mark.asyncio
@@ -53,11 +53,11 @@ async def test_tap_async_with_success(tap_success_response, relay_call):
     assert not action.completed
     assert action.source_device == json.loads('{"type":"rtp","params":{"addr":"10.10.10.10","port":30030,"codec":"PCMU","ptime":20,"rate":8000}}')
     # Complete the action now..
-    payload = json.loads('{"event_type":"calling.call.tap","params":{"control_id":"control-id","call_id":"call-id","node_id":"node-id","state":"finished","tap":{"type":"audio","params":{"direction":"listen"}},"device":{"type":"rtp","params":{"addr":"127.0.0.1","port":"1234","codec":"PCMU","ptime":"20"}}}}')
+    payload = json.loads('{"event_type":"calling.call.tap","params":{"control_id":"control-id","call_id":"call-id","node_id":"node-id","state":"finished","tap":{"type":"audio","params":{"direction":"listen"}},"device":{"type":"rtp","params":{"addr":"127.0.0.1","port":1234,"codec":"PCMU","ptime":20}}}}')
     await _fire(relay_call.calling, payload)
     assert action.completed
     msg = relay_call.calling.client.execute.mock.call_args[0][0]
-    assert msg.params == json.loads('{"protocol":"signalwire-proto-test","method":"calling.tap","params":{"call_id":"call-id","node_id":"node-id","control_id":"control-id","tap":{"type":"audio","params":{"direction":"listen"}},"device":{"type":"rtp","params":{"addr":"127.0.0.1","port":"1234"}}}}')
+    assert msg.params == json.loads('{"protocol":"signalwire-proto-test","method":"calling.tap","params":{"call_id":"call-id","node_id":"node-id","control_id":"control-id","tap":{"type":"audio","params":{"direction":"listen"}},"device":{"type":"rtp","params":{"addr":"127.0.0.1","port":1234}}}}')
     relay_call.calling.client.execute.mock.assert_called_once()
 
 @pytest.mark.asyncio
